8b9705e520f3ea4931892b39cc5c1d19618c342d
habiba <daniyal.aptech96@gmail.com>
2024-03-07 20:40:25 +0500

    Loops PHP

---
+++ loops.php
@@ -0,0 +1,23 @@
+<?php
+
+// LOOPS
+// For Loop
+
+for($i=0; $i<=10; $i++){
+    echo "<br> This is for Loop" .$i;
+}
+
+// Table
+
+for($q=1; $q<=10; $q++){
+    echo "<br> 5 x $q =" .$q*5;
+}
+
+// Table with num
+
+$tableNum = 64;
+for($w=1; $w<=10; $w++){
+    echo "<br> $tableNum x $w = " .$tableNum*$w;
+}
+
+?>
